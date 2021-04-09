045a85a3658791c64ee185c3dc549ce76dc50320
jukka <jukka4996@gmail.com>
2021-04-08 21:33:53 -0300

    created header component

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Inicio | ig.new</title>
+      </Head>
+      <h1>Hellow Word</h1>
+    </>
+  );
+}
