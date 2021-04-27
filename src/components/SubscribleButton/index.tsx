e41c3f7c78e8e4a4499c8ccbae5e3a84132563d9
jukka <jukka4996@gmail.com>
2021-04-26 22:00:32 -0300

    payment with stripe

--- src/components/SubscribleButton/index.tsx
+++ src/components/SubscribleButton/index.tsx
@@ -1,3 +1,6 @@
+import { signIn, useSession } from "next-auth/client";
+import { api } from "../../services/api";
+import { getStripeJS } from "../../services/stripe-js";
 import styles from "./styles.module.scss";
 
 interface ButtonProps {
@@ -5,8 +8,32 @@ interface ButtonProps {
 }
 
 export function SubscriblButton({ priceId }) {
+  const [session] = useSession();
+
+  async function handleSubscrible() {
+    if (!session) {
+      signIn("github");
+      return;
+    }
+
+    try {
+      const { data } = await api.post("subscrible");
+      const { sessionId } = data;
+
+      const stripe = await getStripeJS();
+
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (err) {
+      alert(err.message);
+    }
+  }
+
   return (
-    <button type="button" className={styles.subscribleButton}>
+    <button
+      type="button"
+      className={styles.subscribleButton}
+      onClick={handleSubscrible}
+    >
       Subscrible now
     </button>
   );
